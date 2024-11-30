7a5f5c4f412739f62c4ad9930f8a572bd07ff241
RAIMAPAULSON <raimapaulson11@gmail.com>
2024-12-01 00:37:34 +0530

    project completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { MealManagementComponent } from './meal-management/meal-management.component';
+import { ExpenseManagementComponent } from './expense-management/expense-management.component';
+import { ReportComponent } from './report/report.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserManagementComponent },
+  { path: 'meals', component: MealManagementComponent },
+  { path: 'expenses', component: ExpenseManagementComponent },
+  { path: 'report', component: ReportComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/users' } // Wildcard route for invalid URLs
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { MealManagementComponent } from './meal-management/meal-management.component';
+import { ExpenseManagementComponent } from './expense-management/expense-management.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserManagementComponent,
+    MealManagementComponent,
+    ExpenseManagementComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
